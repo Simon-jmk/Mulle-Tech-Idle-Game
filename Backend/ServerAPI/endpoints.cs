876dcd4961eade04371e9319726230fbba783d06
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2026-02-27 16:09:48 +0100

    ADDED enponits

--- Backend/ServerAPI/endpoints.cs
+++ Backend/ServerAPI/endpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Backend.Data;
+using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 namespace Backend.ServerAPI
 {
@@ -6,8 +7,54 @@ namespace Backend.ServerAPI
     {
         public static void MapEndpoints(this WebApplication app)
         {
-            
-        }
+            // Goal Endpoints
+            app.MapPost("/set/goal/", (IdleLoggerDbContext db, UserGoal goal) =>
+             {
 
+             });
+
+            app.MapPut("/update/goal/", (IdleLoggerDbContext db,  UserGoal goal) =>
+            {
+            });
+
+            app.MapDelete("/delete/goal/{id}", (IdleLoggerDbContext db, ulong id) =>
+            {
+            });
+            app.MapGet("/get/goals/{id}", (IdleLoggerDbContext db, ulong id) =>
+           {
+           });
+
+            app.MapGet("/get/gamestate", (IdleLoggerDbContext db, GameState gameState) => 
+            {
+            });
+            
+            app.MapGet("/get/streak", (IdleLoggerDbContext db, StreakMilestone streak) => 
+            {
+
+            });
+
+
+            app.MapPut("/complete/goal", (IdleLoggerDbContext db, UserGoal goal) =>
+            {
+            });
+
+
+            // User Endpoints
+
+            app.MapGet("/get/user/{id}", (IdleLoggerDbContext db, ulong id) =>
+            {
+            });
+
+            app.MapPost("/create/user", (IdleLoggerDbContext db, User user) =>
+            {
+            });
+
+
+
+
+
+
+        }
     }
 }
+
