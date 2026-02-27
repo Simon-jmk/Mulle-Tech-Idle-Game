e06033a46e361362289a60a17bb4f9fbdc7eeda5
Abdikhani Abdirashid <Abdikhani_abdirashd@hotmail.com>
2026-02-27 10:20:23 +0100

    Add Vite React project

---
+++ src/form.tsx
@@ -0,0 +1,23 @@
+
+function healthForms() {
+
+
+    <form action="">
+
+<label htmlFor="">email</label>
+<input type="email" name="" id="" /> <label htmlFor="">password</label>
+<input type="password" />
+
+
+
+
+    </form>
+
+
+
+}
+
+
+
+
+export default healthForms
