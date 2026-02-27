05ef5bbf9b256880151b8e1c7e477756af6d5c91
Alex <alexcoh19@outlook.com>
2026-02-27 16:06:07 +0100

    Added UserGoal services

---
+++ backend/DTO/UserGoalDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlTypes;
+
+namespace Backend.DTO
+{
+    public class UserGoalDTO
+    {
+        public ulong UserId { get; set; }
+        public string Type { get; set; } = null!;
+        public SqlDecimal TargetValue { get; set; }
+        public SqlDecimal CurrentValue { get; set; }
+        public DateTime? CompletedAt { get; set; } = null;
+    }
+}
